83c5301e84faf76223632f43dd5edae01d68261c
emanuel <emanuel@emanuel-desktop>
2016-05-04 19:03:48 +0200

    Bild Steganography

--- It-Projekt2016/src/BildSteganography/BildSteganography.java
+++ It-Projekt2016/src/BildSteganography/BildSteganography.java
@@ -1,52 +1,144 @@
 package BildSteganography;
 
-import java.awt.Image;
 import java.awt.image.*;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-
 import javax.imageio.ImageIO;
-
-import com.sun.javafx.tk.Toolkit;
-
-import Test.Audiostream;
 
 public class BildSteganography {
 
+	public BildSteganography() {
 
-	public BildSteganography()  {
-
-		
 	}
 
-	public byte[] readImage() throws URISyntaxException, IOException {
+	public int[] readImage(File img) throws IOException {
 
-		URI uri;
-		uri = BildSteganography.class.getResource("/Pictures/cat.jpg").toURI();
-		File img = new File(uri);
 		BufferedImage bufferedImage = ImageIO.read(img);
-		WritableRaster raster = bufferedImage .getRaster();
-		DataBufferByte data = (DataBufferByte) raster.getDataBuffer();
-		return data.getData();
+		int w = bufferedImage.getWidth();
+		int h = bufferedImage.getHeight();
+
+		int[] pixel = bufferedImage.getRGB(0, 0, w, h, null, 0, w);
+
+		return pixel;
 
 	}
-	
-	public void writeImage(byte[] dataByte) throws IOException, URISyntaxException {
+
+	public void hideText(String text, File img) throws IOException {
+
+		BufferedImage bufferedImage = ImageIO.read(img);
+		int w = bufferedImage.getWidth();
+		int h = bufferedImage.getHeight();
+
+		int stelle = 0;
+
+		int[] textBits = new int[text.length() + 7];
+		textBits = bitsInText(text);
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if (textBits.length <= stelle) {
+					break;
+				}
+
+				bufferedImage.setRGB(j, i, setLastBit(bufferedImage.getRGB(j, i), textBits[stelle]));
+				stelle++;
+
+			}
+		}
+
+		ImageIO.write(bufferedImage, "png", img);
+
+	}
+
+	public String extractText(File img) throws IOException {
+
+		BufferedImage bufferedImage = ImageIO.read(img);
+		int w = bufferedImage.getWidth();
+		int h = bufferedImage.getHeight();
+		String chr = "";
+		int anzEinser = 0;
+
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+
+				if (anzEinser >= 6) {
+					break;
+				}
+
+				chr += Integer.toBinaryString(bufferedImage.getRGB(j, i)).charAt(31);
+
+				if (Integer.toBinaryString(bufferedImage.getRGB(j, i)).charAt(31) == '1') {
+					anzEinser++;
+				} else {
+					anzEinser = 0;
+				}
+			}
+		}
+
+		int[] textBits = new int[chr.length()];
+
+		for (int i = 0; i < chr.length(); i++) {
+			textBits[i] = Integer.parseInt(chr.charAt(i) + "");
+		}
+
+		String text = bitsToText(textBits);
+
+		text = text.substring(0, text.length() - 1);
+
+		return text;
+	}
+
+	public int setLastBit(int value, int bit) {
+
+		if (value % 2 == 1 | value % 2 == -1) {
+			value--;
+		}
+
+		value = value + bit;
+
+		return value;
+
+	}
+
+	public int[] bitsInText(String text) {
+
+		int[] bitsInText = new int[text.length() * 8 + 7];
+		String chr;
+		for (int i = 0; i < text.length(); i++) {
+			chr = Integer.toBinaryString((int) text.charAt(i));
+			if (chr.length() < 7) {
+				chr = "0" +chr;
+			}
+			for (int j = 0; j < 7; j++) {
+				bitsInText[(i * 7) + j] = Integer.parseInt(chr.charAt(j) + "");
+			}
+		}
+
+		for (int i = text.length() * 8; i < bitsInText.length; i++) {
+			bitsInText[i] = 1;
+		}
+
+		for (int i = 0; i < bitsInText.length; i++) {
+		}
 		
-		URI uri = BildSteganography.class.getResource("/Pictures").toURI();
-		File outImg = new File(uri.toString(), "/catNew.jpg");
-		
-		ByteArrayInputStream bin = new ByteArrayInputStream(dataByte);
-		BufferedImage img = ImageIO.read(bin);
-		System.out.println(img);
-		ImageIO.write(img, "jpg", outImg);
-		
-		
+		return bitsInText;
+
+	}
+
+	public String bitsToText(int[] bits) {
+
+		String chr = "";
+		String text = "";
+		for (int i = 0; i < bits.length / 7; i++) {
+			for (int j = 0; j < 7; j++) {
+				chr += bits[(i * 7) + j];
+			}
+			text += (char) Integer.parseInt(chr, 2);
+			chr = "";
+		}
+
+		return text;
+
 	}
 
 }
--- It-Projekt2016/src/BildSteganography/Startklasse.java
+++ It-Projekt2016/src/BildSteganography/Startklasse.java
@@ -1,27 +1,41 @@
 package BildSteganography;
 
+import java.awt.Color;
+import java.awt.event.FocusAdapter;
+import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
+
+import sun.misc.FpUtils;
 
 public class Startklasse {
 
 	public static void main(String[] args) {
 		
-		BildSteganography bildSteganography = null;
-
-		bildSteganography = new BildSteganography();
-		
+		BildSteganography bildSteganography = new BildSteganography();
+		URI uri = null;
 		try {
-			byte[] data = bildSteganography.readImage();
-			bildSteganography.writeImage(data);
+			uri = BildSteganography.class.getResource("/Pictures/CuteChick.jpg").toURI();
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+		
+		File img = new File(uri);
+		try {
+			int[] pixel = bildSteganography.readImage(img);
+			bildSteganography.hideText("test", img);
+			String text = bildSteganography.extractText(img);
+			System.out.println(text);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		
+
 	}
 	
 	
